b91666b90cf08447b03a67aca363bfa91573ef3e
¥@K$#!T <92084737+YAKSHIT-22@users.noreply.github.com>
2021-10-10 23:01:56 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,15 @@
+<a href="#"><img width="100%" height="60%" src="th.jpg" height="175px"/></a>
+<h2 align="center">😄፠R፠O፠C፠K፠ ፠P፠A፠P፠E፠R፠ ፠S፠C፠I፠S፠S፠O፠R፠S፠😃</h2>
+<h3 align="center">😎This Is An Rock_Scissors_Game_Against_AI🤖</h3>
+<br>
+<p align="center">
+<h2>Usage😁:-
+<ul>
+<li>git clone https://github.com/YAKSHIT-22/Rock_Scissors_Game_Against_AI</li>
+<li>cd Rock_Scissors_Game_Against_AI</li>
+<li>./rock_scissors_game.py</li>
+</ul>
+😀
+<br>
+</p>
+<h2>Feel Free To Play This Game.(●'◡'●) And Please Support Me By Giving Star..🎇</h2>
---
+++ rock_scissors_game.py
@@ -0,0 +1,76 @@
+import random
+import os, time
+game_list=['Rock','Paper','Scissors']
+computer=c=0
+command=p=0
+x="""\033[1;32;40m\n                                                                                                                                                                                                  
+╔═══╗╔═══╗╔═══╗╔╗╔═╗    ╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗    ╔═══╗╔═══╗╔══╗╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗
+║╔═╗║║╔═╗║║╔═╗║║║║╔╝    ║╔═╗║║╔═╗║║╔═╗║║╔══╝║╔═╗║    ║╔═╗║║╔═╗║╚╣╠╝║╔═╗║║╔═╗║║╔═╗║║╔═╗║║╔═╗║
+║╚═╝║║║ ║║║║ ╚╝║╚╝╝     ║╚═╝║║║ ║║║╚═╝║║╚══╗║╚═╝║    ║╚══╗║║ ╚╝ ║║ ║╚══╗║╚══╗║║ ║║║╚═╝║║╚══╗
+║╔╗╔╝║║ ║║║║ ╔╗║╔╗║     ║╔══╝║╚═╝║║╔══╝║╔══╝║╔╗╔╝    ╚══╗║║║ ╔╗ ║║ ╚══╗║╚══╗║║║ ║║║╔╗╔╝╚══╗║
+║║║╚╗║╚═╝║║╚═╝║║║║╚╗    ║║   ║╔═╗║║║   ║╚══╗║║║╚╗    ║╚═╝║║╚═╝║╔╣╠╗║╚═╝║║╚═╝║║╚═╝║║║║╚╗║╚═╝║
+╚╝╚═╝╚═══╝╚═══╝╚╝╚═╝    ╚╝   ╚╝ ╚╝╚╝   ╚═══╝╚╝╚═╝    ╚═══╝╚═══╝╚══╝╚═══╝╚═══╝╚═══╝╚╝╚═╝╚═══╝
+"""
+y=0
+while y <=len(x):
+    os.system('cls')
+    print (x[:y])
+    time.sleep(.01)
+    y=y+1
+
+
+run=True
+while run:
+    computer_choice= random.choice(game_list)
+    command=input("\033[1;34;40m\nRock, Paper, Scissors or Quit: ")
+    if command == computer_choice:
+        print("Tie")
+    elif command == 'Rock':
+        if computer_choice== 'Scissors':
+             print('Player Won!')
+             p+=1
+             print("Player: " +command)
+             print("Computer: "+computer_choice)
+             print("")
+        else:
+            print("Computer Won!")
+            c+=1
+            print("Player: " +command)
+            print("Computer: "+computer_choice)
+            print("")
+    elif command=='Paper':
+        if computer_choice =='Rock':
+            print('Player Won')
+            p+=1
+            print("Player: " +command)
+            print("Computer: "+computer_choice)
+            print("")
+        else:
+            print("Computer Won")
+            c+=1
+            print("Player: " +command)
+            print("Computer: "+computer_choice)
+            print("")
+    elif command=='Scissors':
+        if computer_choice=='Paper':
+            print('Player Won')
+            p+=1
+            print("Player: " +command)
+            print("Computer: "+computer_choice)
+            print("")
+        else:
+            print("Computer Won")
+            c+=1
+            print("Player: " +command)
+            print("Computer: "+computer_choice)
+            print("")
+    elif command=='Quit':
+           break
+    else:
+        print('\033[1;31;40m\nWrong Command')
+print("Player: " +command)
+print("Computer: "+computer_choice)
+print("")
+print("\033[1;31;40m\nScore: Computer: " + str(c) + "  PLayer: " + str(p))
+print("")
+
